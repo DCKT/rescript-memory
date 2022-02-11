17240315ef5fa322086c4ca9e21967ba42c344ea
Thomas Deconinck <thomas.deconinck@colisweb.com>
2022-02-11 17:47:10 +0100

    chore: clean unused code

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,10 @@
-/// <reference types="vitest" />
 /// <reference types="vite/client" />
 
-import { defineConfig } from 'vite';
-import reactRefresh from '@vitejs/plugin-react-refresh';
-import createReScriptPlugin from '@jihchi/vite-plugin-rescript';
+import { defineConfig } from "vite";
+import reactRefresh from "@vitejs/plugin-react-refresh";
+import createReScriptPlugin from "@jihchi/vite-plugin-rescript";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [reactRefresh(), createReScriptPlugin()],
-  test: {
-    include: ['tests/**/*_test.bs.js'],
-    globals: true,
-    environment: 'jsdom',
-    setupFiles: './tests/setup.ts',
-  },
 });
